c0230f801f492a68312960590eb51e627aaa3694
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-21 11:30:10 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+#Codewars Katas
+
+* My solutions to selected Codewars katas - https://www.codewars.com/users/PaigeRuppel
+
--- src/main/java/com/paigeruppel/codewars/arrays/LowerCaseConvert.java
+++ src/main/java/com/paigeruppel/codewars/arrays/LowerCaseConvert.java
@@ -2,6 +2,11 @@ package com.paigeruppel.codewars.arrays;
 
 public class LowerCaseConvert {
 
+	/*
+	 * Create a function to lowercase all strings in an array. Non-string items
+	 * should remain unchanged.
+	 */
+	
 	public Object[] convertToLowerCase(Object[] items) {
 		for (int i = 0; i < items.length; i++) {
 			if (isString(items[i])) {
@@ -12,7 +17,7 @@ public class LowerCaseConvert {
 		}
 		return items;
 	}
-	
+
 	public static Integer tryParse(String text) {
 		try {
 			return Integer.parseInt(text);
@@ -20,7 +25,7 @@ public class LowerCaseConvert {
 			return null;
 		}
 	}
-	
+
 	private boolean isString(Object item) {
 		String itemAsString = item.toString();
 		return tryParse(itemAsString) == null;
