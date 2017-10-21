0c3b0261ae82c1003cc385b27f508b91d32e6891
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-21 15:00:10 -0400

    more refactoring in fourth instance method

--- src/main/java/com/paigeruppel/codewars/skyscrapers/SkyScraperFourByFour.java
+++ src/main/java/com/paigeruppel/codewars/skyscrapers/SkyScraperFourByFour.java
@@ -3,15 +3,13 @@ package com.paigeruppel.codewars.skyscrapers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.mockito.cglib.core.Converter;
-
 public class SkyScraperFourByFour {
 
 	/*
 	 * https://www.codewars.com/kata/4-by-4-skyscrapers/
 	 */
 	private SkyScraperFourByFourGuideLines guide = new SkyScraperFourByFourGuideLines();
-	
+
 	public List<Integer> locateAllOnes(int[] clues) {
 		List<Integer> onePositions = new ArrayList<>();
 		for (int i = 0; i < clues.length; i++) {
@@ -33,35 +31,36 @@ public class SkyScraperFourByFour {
 		return withFours;
 	}
 
-	//TODO Refactor this monstrous method
 	public int[][] fillInFourthInstanceOfNumber(int[][] incompleteGrid, int numberOfInterest) {
-		List<Integer> filledRows = new ArrayList<>();
-		List<Integer> filledCols = new ArrayList<>();
-		while (filledRows.size() < 3 && filledCols.size() < 3) {
-			for (int row = 0; row < incompleteGrid.length; row++) {
-				for (int col = 0; col < incompleteGrid[0].length; col++) {
-					if (incompleteGrid[row][col] == numberOfInterest) {
-						filledRows.add(row);
-						filledCols.add(col);
-					}
+		int rowCount = 0;
+		int colCount = 0;
+		for (int row = 0; row < incompleteGrid.length; row++) {
+			for (int col = 0; col < incompleteGrid[0].length; col++) {
+				if (incompleteGrid[row][col] == numberOfInterest) {
+					rowCount += row;
+					colCount += col;
 				}
 			}
 		}
-		
-		int remainingRow = 0;
-		int remainingCol = 0;
-		
-		for (int i = 1; i < 4; i++) {
-			if (!filledRows.contains(i)) {
-				remainingRow = i;
-			}
-			if (!filledCols.contains(i)) {
-				remainingCol = i;
-			}
-		}
-		
+		int remainingRow = calculateRemainingPosition(rowCount);
+		int remainingCol = calculateRemainingPosition(colCount);
 		incompleteGrid[remainingRow][remainingCol] = numberOfInterest;
 		return incompleteGrid;
+	}
+	
+	private int calculateRemainingPosition(int count) {
+		int remaining = 0;
+		
+		if (count == 3) {
+			remaining = 3;
+			}
+		if (count == 4) {
+			remaining = 2;
+		}
+		if (count == 5) {
+			remaining = 1;
+		}
+		return remaining;
 	}
 
 }
